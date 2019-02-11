ca64f6dabe836bb38a4b6918422308d6beb49402
Spencer Putt <sputt@alumni.iu.edu>
2019-02-10 23:25:59 -0800

    Add some extra setup details

--- setup.py
+++ setup.py
@@ -5,12 +5,18 @@ setup(
     version='0.1.0',
     author='Spencer Putt',
     author_email='sputt@alumni.iu.edu',
+    description='Python requirements compiler',
     install_requires=open('requirements.txt').readlines(),
     packages=find_packages(include=['qer*']),
+    license='Public Domain',
     entry_points={
         'console_scripts': [
             'req-compile = qer.cmdline:compile_main',
             'req-hash = qer.hash:hash_main',
         ],
-    }
+    },
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Operating System :: OS Independent",
+    ],
 )
