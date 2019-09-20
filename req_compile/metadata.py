52c9efd5cdc191bce5f787063c2d8ce7243dcce0
Spencer Putt <sputt@alumni.iu.edu>
2019-09-19 22:34:08 -0700

    Disable pylint error

--- req_compile/metadata.py
+++ req_compile/metadata.py
@@ -493,6 +493,7 @@ class FakeCython(ModuleType):
         return FakeCython(item)
 
 
+# pylint: disable=too-many-branches
 def _parse_setup_py(name, fake_setupdir, opener, mock_import):  # pylint: disable=too-many-locals,too-many-statements
     # pylint: disable=no-name-in-module,no-member
     # Capture warnings.warn, which is sometimes used in setup.py files
