12c14a06dbd3a992c5af8d697ba0ae00eb47a2df
Spencer Putt <sputt@alumni.iu.edu>
2019-09-27 00:47:44 -0700

    Fix a test that was unintentionally platform specific

--- tests/source-packages/post-3.2.1-1/setup.py
+++ tests/source-packages/post-3.2.1-1/setup.py
@@ -5,10 +5,6 @@ from distutils.core import setup
 
 
 def main():
-    if not (3,) > sys.version_info >= (2, 7):
-        sys.stderr.write('This backport is for Python 2.7 only.\n')
-        sys.exit(1)
-
     setup(
       name='post',
       version='3.2.1-1',
