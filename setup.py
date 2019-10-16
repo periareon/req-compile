4ce51932e89788e8e117e6704a3d152488f0a921
Spencer Putt <sputt@alumni.iu.edu>
2019-10-16 00:53:51 -0700

    Bump version for build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='req-compile',
-    version='0.9.9',
+    version='0.9.9post1',
     author='Spencer Putt',
     author_email='sputt@alumni.iu.edu',
     description='Python requirements compiler',
