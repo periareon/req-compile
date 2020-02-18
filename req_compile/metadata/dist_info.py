e7cbfc0dff75eff664356684ecde42226c3cd550
Spencer Putt <sputt@alumni.iu.edu>
2020-02-17 18:07:26 -0800

    Improve support for wheel files with vendoered packages

--- req_compile/metadata/dist_info.py
+++ req_compile/metadata/dist_info.py
@@ -1,3 +1,6 @@
+import logging
+import os
+import re
 import zipfile
 from contextlib import closing
 
@@ -5,14 +8,50 @@ from req_compile import utils
 from req_compile.dists import DistInfo
 
 
+LOG = logging.getLogger('req_compile.metadata.dist_info')
+
+
+def _find_dist_info_metadata(project_name, namelist):
+    """
+    In a list of zip path entries, find the one that matches the dist-info for this project
+
+    Args:
+        project_name (str): Project name to match
+        namelist (list[str]): List of zip paths
+
+    Returns:
+        (str) The best zip path that matches this project
+    """
+    for best_match in (r'^(.+/)?{}-.+\.dist-info/METADATA$'.format(project_name), r'^.*\.dist-info/METADATA'):
+        for info in namelist:
+            if re.match(best_match, info):
+                LOG.debug('Found dist-info in the zip: %s (with regex %s)', info, best_match)
+                return info
+
+    return None
+
+
 def _fetch_from_wheel(wheel):
+    """
+    Fetch metadata from a wheel file
+    Args:
+        wheel (str): Wheel filename
+
+    Returns:
+        (DistInfo, None) The metadata for this zip, or None if it could not be found or parsed
+    """
+    project_name = os.path.basename(wheel).split('-')[0]
+
     zfile = zipfile.ZipFile(wheel, 'r')
     with closing(zfile):
-        infos = zfile.namelist()
-        for info in infos:
-            if info.endswith('.dist-info/METADATA'):
-                return _parse_flat_metadata(zfile.read(info).decode('utf-8', 'ignore'))
+        # Reverse since metadata details are supposed to be written at the end of the zip
+        infos = reversed(zfile.namelist())
+        print(zfile.namelist())
+        result = _find_dist_info_metadata(project_name, infos)
+        if result is not None:
+            return _parse_flat_metadata(zfile.read(result).decode('utf-8', 'ignore'))
 
+        LOG.warning('Could not find .dist-info/METADATA in the zip archive')
         return None
 
 
---
+++ tests/metadata/test_dist_info.py
@@ -0,0 +1,29 @@
+import os
+
+from req_compile.metadata.dist_info import _find_dist_info_metadata
+
+
+def test_wheel_with_vendored():
+    """Verify that a wheel that contains vendored dist-info directories uses the correct one"""
+    namelist_raw = open(os.path.join(os.path.dirname(__file__), 'pex-listing.txt')).readlines()
+    namelist = [name.strip() for name in namelist_raw]
+
+    assert _find_dist_info_metadata('pex', namelist) == 'pex-2.1.3.dist-info/METADATA'
+    assert _find_dist_info_metadata('pex', list(reversed(namelist))) == 'pex-2.1.3.dist-info/METADATA'
+
+
+def test_normal_wheel():
+    assert _find_dist_info_metadata('normal', ['normal/__init__.py',
+                                               'metadata',
+                                               'normal.dist-info/METADATA']) == 'normal.dist-info/METADATA'
+
+
+def test_best_effort_match():
+    """If the wheel file name is wrong the correct project info can still be found"""
+    assert _find_dist_info_metadata('bad', ['normal/__init__.py',
+                                            'normal.dist-info/METADATA']) == 'normal.dist-info/METADATA'
+
+
+def test_not_found():
+    """Bad zips won't have any metadata"""
+    assert _find_dist_info_metadata('bad', ['totally', 'wrong', 'files']) is None
