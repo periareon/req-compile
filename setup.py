3d6d6f4f462188250f3fe5d0139c8f5737fca73a
Spencer Putt <sputt@alumni.iu.edu>
2020-10-28 00:41:58 -0700

    Bump version as partial build uploaded a wheel

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="req-compile",
-    version="0.10.11",
+    version="0.10.12",
     author="Spencer Putt",
     author_email="sputt@alumni.iu.edu",
     description="Python requirements compiler",
