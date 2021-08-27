db590166d1b4f213173eafae33ca4a67acf3b1b4
Spencer Putt <spencer.putt@spacex.com>
2021-08-27 01:17:26 -0700

    Drop python 2 classifier

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
