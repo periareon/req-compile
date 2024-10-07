10b49b28dead22ffe62c38fdbb4de9d785ed6f76
Andre Brisco <andre.brisco@protonmail.com>
2024-10-07 13:58:41 -0700

    Avoid build-without-the-bytes issue on Windows.

---
+++ private/copier.py
@@ -0,0 +1,28 @@
+"""A script used to avoid https://github.com/bazelbuild/bazel/issues/21747
+"""
+
+import argparse
+import shutil
+from pathlib import Path
+
+
+def parse_args() -> argparse.Namespace:
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("src", type=Path, help="The source file.")
+    parser.add_argument("dest", type=Path, help="The destination path.")
+
+    return parser.parse_args()
+
+
+def main() -> None:
+    """The main entrypoint"""
+    args = parse_args()
+
+    args.dest.parent.mkdir(exist_ok=True, parents=True)
+    shutil.copy2(args.src, args.dest)
+
+
+if __name__ == "__main__":
+    main()
